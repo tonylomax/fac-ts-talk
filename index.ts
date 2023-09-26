73390da47ff85ca384e55c0fe905b75a603613a7
Tony Lomax <tonylomax@tonys-mbp.home>
2023-09-26 10:17:38 +0100

    add talk example files

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { main } from "./todo";
+
+const add = (number1: number, number2: number) => {
+  return number1 + number2;
+};
+
+// console.log(add(2, 2));
+// console.log(add(2, "2"));
+// console.log(add(2, "foo"));
+// console.log(add(2, {}));
+// console.log(add(2));
+main();
---
+++ todo.ts
@@ -0,0 +1,29 @@
+type Todo = {
+  title: string;
+  description?: string;
+};
+
+const todo1 = {
+  title: "do a FAC talk",
+  description: "find gifs",
+};
+
+// const todo2: Todo = {
+//   title: "learn python",
+//   id: 2,
+// };
+
+const updateTodo = (todo: Todo, fieldsToUpdate: Partial<Todo>) => {
+  return { ...todo, ...fieldsToUpdate };
+};
+
+export const main = () => {
+  const newTodo = updateTodo(todo1, { title: "new todo" });
+
+  console.log({ newTodo });
+};
+
+// const name = "tony" as unknown as number;
+// Excess Property Checks
+// In this case, Typescript won’t check for excess properties as type Person is a subset of the type inferred for variable x, which is inferred to include all properties in Person plus age. This is known as duck-typing or structural typing, which I will look at later.
+// "If it walks like a duck and it quacks like a duck, then it must be a duck"
