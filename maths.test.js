031b4f9e32e4774ac39285cd4dd578b5edea7f2c
tonylomax <tony.lomax@gmail.com>
2023-10-25 13:22:46 +0100

    changes

--- maths.test.js
+++ maths.test.js
@@ -2,7 +2,7 @@ import { add } from "./maths";
 
 describe("add", () => {
   test("should return a number", () => {
-    const result = add(2, 2);
+    const result = add(2);
 
     expect(typeof result).toBe("number");
   });
